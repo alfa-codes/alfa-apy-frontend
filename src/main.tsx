6204c3bc48febb3805a232663221e6ace3ed0b23
Diukarev Oleksandr <dyukit99@gmail.com>
2025-05-16 19:57:39 +0700

    target canister changed

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")!).render(
     <ToastContainer />
     <Provider store={store}>
       <IdentityKitProvider
-        signerClientOptions={{ targets: ["do25a-dyaaa-aaaak-qifua-cai"] }}
+        signerClientOptions={{ targets: ["ownab-uaaaa-aaaap-qp2na-cai"] }}
       >
         <App />
       </IdentityKitProvider>
