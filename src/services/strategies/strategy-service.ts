3bdab86d358ce3ba4e0d9c8799a978c32d3ed934
v7m <vadymladimyrych@Vadyms-personal.local>
2025-06-06 01:40:30 +0200

    Add withdraw and accept_investment to strategy service

--- src/services/strategies/strategy-service.ts
+++ src/services/strategies/strategy-service.ts
@@ -3,6 +3,8 @@ import {
   _SERVICE as VaultType,
   StrategyResponse,
   UserStrategyResponse,
+  DepositResponse,
+  WithdrawResponse,
 } from "../../idl/vault";
 import { idlFactory } from "../../idl/vault_idl";
 import { Principal } from "@dfinity/principal";
@@ -23,58 +25,35 @@ export class StrategiesService {
       idlFactory
     );
     const strategies = await anonymousActor.get_strategies()
-    .then((strategies) => strategies.filter((strategy) => strategy.current_pool.length > 0));
+      .then((strategies) => strategies.filter((strategy) => strategy.current_pool.length > 0));
+
     const poolMetricsArgs = await Promise.all(
       strategies.flatMap((strategy) =>
-        strategy.pools.map(async (pool) => {
+        strategy.pools.map((pool) => {
           return {
-            //TODO: fix this
             provider: pool.provider,
-            token0: strategy.current_pool[0]!.token0,
-            token1: strategy.current_pool[0]!.token1,
+            token0: pool.token0,
+            token1: pool.token1,
           };
         })
       )
     );
     console.log("poolMetricsArgs", poolMetricsArgs);
+
     const poolStats = await poolStatsService.get_pool_metrics(poolMetricsArgs);
     console.log("poolStats", poolStats);
-    // Find the pool with the highest APY
-    const maxApyPoolStat = poolStats.reduce((max, ps) => {
-      return ps[1].apy > max[1].apy ? ps : max;
-    }, poolStats[0]);
-    const maxApyPoolId = getPoolId({
-      provider: maxApyPoolStat[0].provider,
-      token0: maxApyPoolStat[0].token0.symbol,
-      token1: maxApyPoolStat[0].token1.symbol,
-    });
+
     return strategies.map((strategy) => ({
       ...strategy,
-      current_pool: (() => {
-        const found = strategy.pools.find((p) => getPoolId(p) === maxApyPoolId);
-        return found
-          ? [
-              {
-                provider: found.provider,
-                token0: {
-                  symbol: found.token0,
-                  ledger: Principal.fromText("ryjl3-tyaaa-aaaaa-aaaba-cai"),
-                },
-                token1: {
-                  symbol: found.token1,
-                  ledger: Principal.fromText("druyg-tyaaa-aaaaq-aactq-cai"),
-                },
-              },
-            ]
-          : [];
-      })(),
       apy:
         poolStats.find((pool) => {
+          const currentPool = strategy.current_pool[0]!;
+
           return strategy.current_pool.length
             ? getPoolId({
-                provider: maxApyPoolStat[0].provider,
-                token0: maxApyPoolStat[0].token0.symbol,
-                token1: maxApyPoolStat[0].token1.symbol,
+                provider: currentPool.provider,
+                token0: currentPool.token0.symbol,
+                token1: currentPool.token1.symbol,
               }) ===
                 getPoolId({
                   provider: pool[0].provider,
@@ -99,6 +78,40 @@ export class StrategiesService {
     return (await this.get_strategies()).filter((s) =>
       userStrategiesIds.includes(s.id)
     ) as unknown as Array<UserStrategyResponse>;
+  }
+
+  public async accept_investment(
+    strategy_id: number,
+    ledger: string,
+    amount: bigint,
+  ) : Promise<DepositResponse> {
+    const anonymousActor = await getAnonActor<VaultType>(
+      VAULT_CANISTER_ID,
+      idlFactory
+    );
+
+    return await anonymousActor.accept_investment({
+      strategy_id,
+      ledger: Principal.fromText(ledger),
+      amount,
+    });
+  }
+
+  public async withdraw(
+    strategy_id: number,
+    ledger: string,
+    amount: bigint,
+  ) : Promise<WithdrawResponse> {
+    const anonymousActor = await getAnonActor<VaultType>(
+      VAULT_CANISTER_ID,
+      idlFactory
+    );
+
+    return await anonymousActor.withdraw({
+      strategy_id,
+      ledger: Principal.fromText(ledger),
+      amount,
+    });
   }
 }
 
