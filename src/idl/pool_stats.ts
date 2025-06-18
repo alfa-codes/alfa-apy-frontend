bb1283bec3d5e06f245261caea9de2c31510ac6c
v7m <vadymladimyrych@Vadyms-personal.local>
2025-06-18 04:55:25 +0200

    Update canisters IDLs

--- src/idl/pool_stats.ts
+++ src/idl/pool_stats.ts
@@ -11,22 +11,32 @@ export interface ApyValue { 'tokens_apy' : bigint, 'usd_apy' : bigint }
 export type ExchangeId = { 'Sonic' : null } |
   { 'KongSwap' : null } |
   { 'ICPSwap' : null };
+export interface InternalError {
+  'context' : string,
+  'code' : number,
+  'kind' : InternalErrorKind,
+  'extra' : [] | [Array<[string, string]>],
+  'message' : string,
+}
+export type InternalErrorKind = { 'AccessDenied' : null } |
+  { 'Infrastructure' : null } |
+  { 'NotFound' : null } |
+  { 'Timeout' : null } |
+  { 'Unknown' : null } |
+  { 'BusinessLogic' : null } |
+  { 'ExternalService' : null } |
+  { 'Validation' : null };
 export interface Pool {
   'id' : string,
   'provider' : ExchangeId,
-  'initial_position' : [] | [Position],
   'token0' : Principal,
   'token1' : Principal,
+  'position_id' : [] | [bigint],
 }
 export interface PoolApy {
   'month' : ApyValue,
   'week' : ApyValue,
   'year' : ApyValue,
-}
-export interface PoolByTokens {
-  'provider' : ExchangeId,
-  'token0' : Principal,
-  'token1' : Principal,
 }
 export interface PoolData { 'tvl' : bigint }
 export interface PoolMetrics { 'apy' : PoolApy, 'tvl' : bigint }
@@ -43,11 +53,6 @@ export interface PoolSnapshotArgs {
   'pool_id' : string,
   'position_data' : [] | [PositionData],
 }
-export interface Position {
-  'id' : bigint,
-  'initial_amount0' : bigint,
-  'initial_amount1' : bigint,
-}
 export interface PositionData {
   'id' : bigint,
   'usd_amount0' : bigint,
@@ -55,34 +60,49 @@ export interface PositionData {
   'amount0' : bigint,
   'amount1' : bigint,
 }
+export interface ResponseError {
+  'source' : [] | [InternalError],
+  'code' : number,
+  'kind' : InternalErrorKind,
+  'message' : string,
+  'details' : [] | [Array<[string, string]>],
+}
 export type Result = { 'Ok' : AddLiquidityResponse } |
-  { 'Err' : string };
-export type Result_1 = { 'Ok' : WithdrawFromPoolResponse } |
-  { 'Err' : string };
+  { 'Err' : ResponseError };
+export type Result_1 = { 'Ok' : string } |
+  { 'Err' : ResponseError };
+export type Result_2 = { 'Ok' : null } |
+  { 'Err' : ResponseError };
+export type Result_3 = { 'Ok' : Pool } |
+  { 'Err' : ResponseError };
+export type Result_4 = { 'Ok' : Array<Pool> } |
+  { 'Err' : ResponseError };
+export type Result_5 = { 'Ok' : WithdrawFromPoolResponse } |
+  { 'Err' : ResponseError };
 export interface WithdrawFromPoolResponse {
   'token_0_amount' : bigint,
   'token_1_amount' : bigint,
 }
 export interface _SERVICE {
-  'add_liquidity_to_pool' : ActorMethod<[string, bigint], Result>,
-  'add_pool' : ActorMethod<[Principal, Principal, ExchangeId], string>,
+  'add_liquidity_to_pool' : ActorMethod<[Principal, string, bigint], Result>,
+  'add_pool' : ActorMethod<[Principal, Principal, ExchangeId], Result_1>,
   'add_pool_snapshot' : ActorMethod<[PoolSnapshotArgs], undefined>,
+  'create_pool_snapshot' : ActorMethod<[string], PoolSnapshot>,
   'delete_all_pools_and_snapshots' : ActorMethod<[], boolean>,
-  'delete_pool' : ActorMethod<[string], boolean>,
+  'delete_pool' : ActorMethod<[string], Result_2>,
   'delete_pool_snapshot' : ActorMethod<[string, string], undefined>,
   'delete_pool_snapshots' : ActorMethod<[string], undefined>,
-  'get_pool_by_id' : ActorMethod<[string], [] | [Pool]>,
-  'get_pool_by_tokens' : ActorMethod<[PoolByTokens], [] | [Pool]>,
+  'get_pool_by_id' : ActorMethod<[string], Result_3>,
   'get_pool_metrics' : ActorMethod<
     [Array<string>],
     Array<[string, PoolMetrics]>
   >,
-  'get_pools' : ActorMethod<[], Array<Pool>>,
+  'get_pools' : ActorMethod<[], Result_4>,
   'get_pools_snapshots' : ActorMethod<
     [Array<string>],
     Array<[string, Array<PoolSnapshot>]>
   >,
-  'remove_liquidity_from_pool' : ActorMethod<[string], Result_1>,
+  'remove_liquidity_from_pool' : ActorMethod<[string], Result_5>,
   'set_operator' : ActorMethod<[Principal], undefined>,
   'update_pool_ids' : ActorMethod<[], boolean>,
 }
--- src/idl/pool_stats_idl.ts
+++ src/idl/pool_stats_idl.ts
@@ -5,12 +5,40 @@ export const idlFactory = ({ IDL }: { IDL: any }) => {
     'token_0_amount' : IDL.Nat,
     'token_1_amount' : IDL.Nat,
   });
-  const Result = IDL.Variant({ 'Ok' : AddLiquidityResponse, 'Err' : IDL.Text });
+  const InternalErrorKind = IDL.Variant({
+    'AccessDenied' : IDL.Null,
+    'Infrastructure' : IDL.Null,
+    'NotFound' : IDL.Null,
+    'Timeout' : IDL.Null,
+    'Unknown' : IDL.Null,
+    'BusinessLogic' : IDL.Null,
+    'ExternalService' : IDL.Null,
+    'Validation' : IDL.Null,
+  });
+  const InternalError = IDL.Record({
+    'context' : IDL.Text,
+    'code' : IDL.Nat32,
+    'kind' : InternalErrorKind,
+    'extra' : IDL.Opt(IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text))),
+    'message' : IDL.Text,
+  });
+  const ResponseError = IDL.Record({
+    'source' : IDL.Opt(InternalError),
+    'code' : IDL.Nat32,
+    'kind' : InternalErrorKind,
+    'message' : IDL.Text,
+    'details' : IDL.Opt(IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text))),
+  });
+  const Result = IDL.Variant({
+    'Ok' : AddLiquidityResponse,
+    'Err' : ResponseError,
+  });
   const ExchangeId = IDL.Variant({
     'Sonic' : IDL.Null,
     'KongSwap' : IDL.Null,
     'ICPSwap' : IDL.Null,
   });
+  const Result_1 = IDL.Variant({ 'Ok' : IDL.Text, 'Err' : ResponseError });
   const PoolData = IDL.Record({ 'tvl' : IDL.Nat });
   const PositionData = IDL.Record({
     'id' : IDL.Nat,
@@ -25,30 +53,6 @@ export const idlFactory = ({ IDL }: { IDL: any }) => {
     'pool_id' : IDL.Text,
     'position_data' : IDL.Opt(PositionData),
   });
-  const Position = IDL.Record({
-    'id' : IDL.Nat,
-    'initial_amount0' : IDL.Nat,
-    'initial_amount1' : IDL.Nat,
-  });
-  const Pool = IDL.Record({
-    'id' : IDL.Text,
-    'provider' : ExchangeId,
-    'initial_position' : IDL.Opt(Position),
-    'token0' : IDL.Principal,
-    'token1' : IDL.Principal,
-  });
-  const PoolByTokens = IDL.Record({
-    'provider' : ExchangeId,
-    'token0' : IDL.Principal,
-    'token1' : IDL.Principal,
-  });
-  const ApyValue = IDL.Record({ 'tokens_apy' : IDL.Nat, 'usd_apy' : IDL.Nat });
-  const PoolApy = IDL.Record({
-    'month' : ApyValue,
-    'week' : ApyValue,
-    'year' : ApyValue,
-  });
-  const PoolMetrics = IDL.Record({ 'apy' : PoolApy, 'tvl' : IDL.Nat });
   const PoolSnapshot = IDL.Record({
     'id' : IDL.Text,
     'pool_data' : IDL.Opt(PoolData),
@@ -56,40 +60,61 @@ export const idlFactory = ({ IDL }: { IDL: any }) => {
     'pool_id' : IDL.Text,
     'position_data' : IDL.Opt(PositionData),
   });
+  const Result_2 = IDL.Variant({ 'Ok' : IDL.Null, 'Err' : ResponseError });
+  const Pool = IDL.Record({
+    'id' : IDL.Text,
+    'provider' : ExchangeId,
+    'token0' : IDL.Principal,
+    'token1' : IDL.Principal,
+    'position_id' : IDL.Opt(IDL.Nat),
+  });
+  const Result_3 = IDL.Variant({ 'Ok' : Pool, 'Err' : ResponseError });
+  const ApyValue = IDL.Record({ 'tokens_apy' : IDL.Nat, 'usd_apy' : IDL.Nat });
+  const PoolApy = IDL.Record({
+    'month' : ApyValue,
+    'week' : ApyValue,
+    'year' : ApyValue,
+  });
+  const PoolMetrics = IDL.Record({ 'apy' : PoolApy, 'tvl' : IDL.Nat });
+  const Result_4 = IDL.Variant({ 'Ok' : IDL.Vec(Pool), 'Err' : ResponseError });
   const WithdrawFromPoolResponse = IDL.Record({
     'token_0_amount' : IDL.Nat,
     'token_1_amount' : IDL.Nat,
   });
-  const Result_1 = IDL.Variant({
+  const Result_5 = IDL.Variant({
     'Ok' : WithdrawFromPoolResponse,
-    'Err' : IDL.Text,
+    'Err' : ResponseError,
   });
   return IDL.Service({
-    'add_liquidity_to_pool' : IDL.Func([IDL.Text, IDL.Nat], [Result], []),
+    'add_liquidity_to_pool' : IDL.Func(
+        [IDL.Principal, IDL.Text, IDL.Nat],
+        [Result],
+        [],
+      ),
     'add_pool' : IDL.Func(
         [IDL.Principal, IDL.Principal, ExchangeId],
-        [IDL.Text],
+        [Result_1],
         [],
       ),
     'add_pool_snapshot' : IDL.Func([PoolSnapshotArgs], [], []),
+    'create_pool_snapshot' : IDL.Func([IDL.Text], [PoolSnapshot], []),
     'delete_all_pools_and_snapshots' : IDL.Func([], [IDL.Bool], []),
-    'delete_pool' : IDL.Func([IDL.Text], [IDL.Bool], []),
+    'delete_pool' : IDL.Func([IDL.Text], [Result_2], []),
     'delete_pool_snapshot' : IDL.Func([IDL.Text, IDL.Text], [], []),
     'delete_pool_snapshots' : IDL.Func([IDL.Text], [], []),
-    'get_pool_by_id' : IDL.Func([IDL.Text], [IDL.Opt(Pool)], []),
-    'get_pool_by_tokens' : IDL.Func([PoolByTokens], [IDL.Opt(Pool)], []),
+    'get_pool_by_id' : IDL.Func([IDL.Text], [Result_3], []),
     'get_pool_metrics' : IDL.Func(
         [IDL.Vec(IDL.Text)],
         [IDL.Vec(IDL.Tuple(IDL.Text, PoolMetrics))],
         [],
       ),
-    'get_pools' : IDL.Func([], [IDL.Vec(Pool)], []),
+    'get_pools' : IDL.Func([], [Result_4], []),
     'get_pools_snapshots' : IDL.Func(
         [IDL.Vec(IDL.Text)],
         [IDL.Vec(IDL.Tuple(IDL.Text, IDL.Vec(PoolSnapshot)))],
         [],
       ),
-    'remove_liquidity_from_pool' : IDL.Func([IDL.Text], [Result_1], []),
+    'remove_liquidity_from_pool' : IDL.Func([IDL.Text], [Result_5], []),
     'set_operator' : IDL.Func([IDL.Principal], [], []),
     'update_pool_ids' : IDL.Func([], [IDL.Bool], []),
   });
